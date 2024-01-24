07eeafbe54fd9be9772fd59225f186acc5e1f223
Rdean <rdeanewing@gmail.com>
2024-01-24 12:40:51 -0500

    added name sorter listener

--- index.js
+++ index.js
@@ -49,7 +49,11 @@ const park = main.querySelector(".park-display");
 
 // remove that park
 // main.removeChild(park);
-const firstBtn = document.querySelector("button");
-firstBtn.addEventListener("click", (event) => {
-  console.log("You clicked the button", event);
+
+// Select the `nameSorter` link
+const nameSorter = document.querySelector("#name-sorter");
+
+// Add an event listener
+nameSorter.addEventListener("click", (event) => {
+  console.log("You clicked the name sorter");
 });
