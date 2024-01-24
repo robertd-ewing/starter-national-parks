43f1c473f1e8a2d4f65040efeb85ca2e66d3efb3
Rdean <rdeanewing@gmail.com>
2024-01-24 12:44:00 -0500

    finished name sorter

--- index.js
+++ index.js
@@ -51,10 +51,40 @@ const park = main.querySelector(".park-display");
 // main.removeChild(park);
 
 // Select the `nameSorter` link
+// Select the `nameSorter` link
 const nameSorter = document.querySelector("#name-sorter");
 
 // Add an event listener
 nameSorter.addEventListener("click", (event) => {
   event.preventDefault();
-  console.log("You clicked the name sorter");
+
+  // 1. Get the main element
+  const main = document.querySelector("main");
+
+  // 2. Get the list of parks
+  const parksList = main.querySelectorAll(".park-display");
+
+  // 3. Empty the main element
+  main.innerHTML = "";
+
+  // 4. Create an array
+  const parksArray = Array.from(parksList);
+
+  // 5. Sort the array
+  parksArray.sort((parkA, parkB) => {
+    const parkAName = parkA.querySelector("h2").innerText;
+    const parkBName = parkB.querySelector("h2").innerText;
+    if (parkAName < parkBName) {
+      return -1;
+    } else if (parkAName > parkBName) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  // 6. Insert each park into the DOM
+  parksArray.forEach((park) => {
+    main.appendChild(park);
+  });
 });
